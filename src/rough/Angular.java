4bc7a62d0553114a6461f3d43431011e70a82ab9
saurabh <saurabh.kumar@epsilon.com>
2018-05-30 12:18:24 +0530

    What is typscript

---
+++ src/rough/Angular.java
@@ -0,0 +1,13 @@
+Angular
+
+TypeScript is a typed superset of JavaScript that compiles to plain JavaScript. TypeScript is pure object oriented with classes, interfaces and statically typed like C#
+
+TypeScript can help programmers to write object-oriented programs and have them compiled to JavaScript, both on server side and client side.
+
+its failure to embrace the features of Object Orientation, strong type checking and compile-time error checks prevents JavaScript from succeeding at the enterprise level as a full-fledged server-side technology. TypeScript was presented to bridge this gap.
+
+What is TypeScript?
+By definition, “TypeScript is JavaScript for application-scale development.”
+
+TypeScript is a strongly typed, object oriented, compiled language. It was designed by Anders Hejlsberg (designer of C#) at Microsoft. TypeScript is both a language and a set of tools.
+TypeScript is a typed superset of JavaScript compiled to JavaScript. In other words, TypeScript is JavaScript plus some additional features.
