f71dcb61eeede378553fe70e034c7a165a637a73
Saurabh <emosrb@gmail.com>
2020-07-28 08:12:11 +0530

    Added 8

---
+++ src/java8/functionalInterfaces/FunctionStudentExample.java
@@ -0,0 +1,33 @@
+package java8.functionalInterfaces;
+
+import java8.data.Student;
+import java8.data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FunctionStudentExample {
+
+    static Function<List<Student>, Map<String, Double>>  function = (students -> {
+
+        Map<String,Double> studentGradeMap = new HashMap<>();
+        students.forEach((student -> {
+
+            if(PredicateStudentExample.p1.test(student)){
+                studentGradeMap.put(student.getName(),student.getGpa());
+            }
+        }));
+
+        return studentGradeMap;
+
+    });
+
+    public static void main(String[] args) {
+
+        System.out.println(function.apply(StudentDataBase.getAllStudents()));
+
+    }
+}
---
+++ src/java8/methodreferences/SupplierMethodReferenceExample.java
@@ -0,0 +1,16 @@
+package java8.methodreferences;
+
+import java8.data.Student;
+
+import java.util.function.Supplier;
+
+public class SupplierMethodReferenceExample {
+
+    Supplier<Student> studentSupplier = Student::new;
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+}
